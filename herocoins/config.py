7c95324c99177ac2b9d51aec8d3b6a1b3a08569f
MrDoc1 <112913658+MrDoc1@users.noreply.github.com>
2022-12-27 00:59:16 -0600

    Delete config.py

--- herocoins/config.py
+++
@@ -1,6 +0,0 @@
-host = "ec2-44-209-186-51.compute-1.amazonaws.com"
-port = 5432
-database = "d6q1u4ip8257fr"
-user = "xtwoxybknlfosr"
-password = "815c5b0c1570352149d14c770103de6d12b6eb14f3823119527e5e2f6a992def"
-TOKEN = 'MTAwNDkwMTE2MTY4MzM5NDY1Mg.GXewIA.xebTYKgae1iDZcRo5Q993aBStbd4VbHryHjF60'
